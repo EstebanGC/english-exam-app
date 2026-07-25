e85761b3c578925ff7f17839439eabb0527732a8
EstebanGC <johan.gal.car@gmail.com>
2026-07-25 15:14:11 -0500

    Fixing vercel domain

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://enlgish-exam-client-8xori9dav-english-evaluator-exam.vercel.app",
+        "https://enlgish-exam-client-lilac.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
