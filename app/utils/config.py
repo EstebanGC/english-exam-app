26f589e4ed71c73b16b3fdcb87b8d9d2fadcba61
EstebanGC <johan.gal.car@gmail.com>
2026-08-08 09:57:09 -0500

    Deploy 12

--- app/utils/config.py
+++ app/utils/config.py
@@ -20,7 +20,11 @@ if not DATABASE_URL:
         f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
