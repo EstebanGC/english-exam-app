f972e8e1c64b95c7887e30fefc4dd85c061780f4
EstebanGC <johan.gal.car@gmail.com>
2026-08-23 09:59:32 -0500

    Fixing typo

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
-from app.routes import auth_routes, evaluation_routes, history, rubric_routes, speaking_routes
+from app.routes import auth_routes, evaluation_routes, history_routes, rubric_routes, speaking_routes
 
 app = FastAPI()
 
